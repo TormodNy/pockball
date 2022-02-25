aa79d3dc75c69a9f8e842654546548631208b230
TormodNy <tormodn99@gmail.com>
2022-02-25 11:05:42 +0100

    Initial commit

---
+++ android/src/com/pockball/pockball/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pockball.pockball;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pockball.pockball.PockBall;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PockBall(), config);
+	}
+}
