01724c3dc1fbec950ea35b3e638c4518a5b118e4
Sebastian Cheng <sebast1908@gmail.com>
2022-03-06 13:56:06 +0100

    #2 Create ScreenController and ScreenModel

--- core/src/com/pockball/pockball/screens/ScreenModel.java
+++ core/src/com/pockball/pockball/screens/ScreenModel.java
@@ -1,4 +1,9 @@
 package com.pockball.pockball.screens;
 
 public class ScreenModel {
+    // List of all screens in the application
+    public enum Screen {
+        SINGLEPLAYER,
+        MULTIPLAYER
+    }
 }
