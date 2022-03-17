cd595949fb283cc3ea346b482b38f1ed6d2240b4
Sebastian Cheng <sebast1908@gmail.com>
2022-03-17 14:42:32 +0100

    #34 Update settings menu

--- core/src/com/pockball/pockball/screens/Util.java
+++ core/src/com/pockball/pockball/screens/Util.java
@@ -1,17 +1,17 @@
 package com.pockball.pockball.screens;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class Util {
 
-    public static void addPathToButton(ScreenController screenController, TextButton button, ScreenModel.Screen screen) {
+    public static void addPathToButton(ScreenController screenController, TextButton button, ScreenModel.Screen screen, ScreenModel.Screen previousScreen) {
         button.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                screenController.changeScreen(screen);
+                screenController.changeScreen(screen, previousScreen);
+                System.out.println("Trykk");
             }
         });
     }
