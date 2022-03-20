dcdc4570dae8bd5917be147bf018af92bed89fcf
Siguhau <siguhau@stud.ntnu.com>
2022-03-20 21:17:02 +0100

    bugfix/hotfix, repeat screenchange to show board #21

--- core/src/com/pockball/pockball/screens/Util.java
+++ core/src/com/pockball/pockball/screens/Util.java
@@ -11,6 +11,10 @@ public class Util {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 screenController.changeScreen(screen, previousScreen);
+                // TODO: This is a temporary bugfix, that prevents quitting a SP-game,
+                //  then reentering to not show pool board.
+                screenController.changeScreen(previousScreen, screen);
+                screenController.changeScreen(screen, previousScreen);
             }
         });
     }
